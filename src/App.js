786500e7581f014ba5b6129d300e969d4d3640dd
monicamartinez64 <monicamartinezmj@gmail.com>
2020-09-10 21:33:29 -0500

    create home page layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList } from 'react-mdl';
+
 
 function App() {
   return (
     <div>
-      Monica's React Portfolio
-    </div>
+    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    <Footer size="mini">
+    <FooterSection type="left" logo="Title">
+        <FooterLinkList>
+            <a href="#">Help</a>
+            <a href="#">Privacy Terms</a>
+        </FooterLinkList>
+    </FooterSection>
+    </Footer>
+    </Layout>
+       
+</div>
   );
 }
 
