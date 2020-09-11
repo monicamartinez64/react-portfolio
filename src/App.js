17c2c8713fd3c9f5fd07ed7b5f2b806883826061
monicamartinez64 <monicamartinezmj@gmail.com>
2020-09-10 22:18:19 -0500

    set up navigation routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList } from 'react-mdl';
+import Main from './Components/main';
+import { Link } from 'react-router-dom';
 
 
 function App() {
@@ -9,21 +11,26 @@ function App() {
     <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
         <Header transparent title="Title" style={{color: 'white'}}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/projects">My Projects</Link>
+                <Link to="/contact">Contact Me</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+            <Link to="/resume">Resume</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/projects">My Projects</Link>
+                <Link to="/contact">Contact Me</Link>
             </Navigation>
         </Drawer>
-        <Content />
+        <Content>
+          <div className="page-content" />
+          <Main>
+
+          </Main>
+        </Content>
     <Footer size="mini">
     <FooterSection type="left" logo="Title">
         <FooterLinkList>
