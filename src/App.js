19d1aaa5d99e8e617c88a916b6b932072f189ce9
monicamartinez64 <monicamartinezmj@gmail.com>
2020-09-10 22:31:10 -0500

    change background image

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+    <Layout style={{background: 'url(https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center / cover'}}>
         <Header transparent title="Title" style={{color: 'white'}}>
             <Navigation>
                 <Link to="/resume">Resume</Link>
@@ -19,7 +19,7 @@ function App() {
         </Header>
         <Drawer title="Title">
             <Navigation>
-            <Link to="/resume">Resume</Link>
+                <Link to="/resume">Resume</Link>
                 <Link to="/about">About Me</Link>
                 <Link to="/projects">My Projects</Link>
                 <Link to="/contact">Contact Me</Link>
